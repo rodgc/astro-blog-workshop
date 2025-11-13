dac391c3ebb3bea107b0b5f29bfb682ef68a3155
rodgc <rod.gc@hotmail.com>
2025-11-13 03:03:41 -0600

    feat(config): add environment variables for email configuration
    Add required environment variables for email functionality.
    
    This change introduces `RESEND_API_KEY`, `FROM_EMAIL`, and `TO_EMAIL` as mandatory configuration fields. These parameters will facilitate email sending capabilities in the application.
    
    Also updates dependencies to include the `resend` library for handling email services.

--- astro.config.mjs
+++ astro.config.mjs
@@ -14,6 +14,24 @@ export default defineConfig({
   }),
   env: {
     schema: {
+      RESEND_API_KEY: envField.string({
+        context: 'server',
+        access: 'secret',
+        optional: false,
+        default: 'INFORM_VALID_TOKEN',
+      }),
+      FROM_EMAIL: envField.string({
+        context: 'server',
+        access: 'secret',
+        optional: false,
+        default: 'INFORM_VALID_EMAIL',
+      }),
+      TO_EMAIL: envField.string({
+        context: 'server',
+        access: 'secret',
+        optional: false,
+        default: 'INFORM_VALID_EMAIL',
+      }),
       CONTENT_ISLAND_SECRET_TOKEN: envField.string({
         context: 'server',
         access: 'secret',
