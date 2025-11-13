ab49a13583ef4771055ecc10883a41cb3d9b58fa
rodgc <rod.gc@hotmail.com>
2025-11-13 02:14:29 -0600

    feat(mini-bio): add MiniBio interface with media support
    Introduce the MiniBio interface to define the structure for mini bios, including properties such as id, language, title, name, role, description, and image.
    
    This enhancement improves type safety and allows for better integration with media content.

--- src/pods/mini-bio/mini-bio.model.ts
+++ src/pods/mini-bio/mini-bio.model.ts
@@ -1,1 +1,14 @@
 export type MiniBioType = 'hero' | 'card';
+
+import type { Media } from '@content-island/api-client';
+
+export interface MiniBio {
+  id: string;
+  language: 'en';
+  title: string;
+  name: string;
+  role: string;
+  description: string;
+  image: Media;
+  imageAlt: string;
+}
